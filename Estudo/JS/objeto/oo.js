44461786d8f858af4bd2d2b1a5c55c88b69bdd7d
vmfas <124852156+vmfas@users.noreply.github.com>
2023-06-05 12:45:41 -0300

    objeto.js

---
+++ Estudo/JS/objeto/oo.js
@@ -0,0 +1,18 @@
+// código não executável
+
+// procedural
+processamento(valor1, valor2, valor3)
+
+// OO
+objeto = {
+    valor1,
+    valor2,
+    valor3,
+    processamento(){
+        // ...
+    }
+}
+
+objeto.processamento() // Foco passou a ser o objeto
+
+// Princípios importantes
